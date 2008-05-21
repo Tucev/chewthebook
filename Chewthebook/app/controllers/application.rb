19d4c4eaf002f008d675e9d76b2a2c38fe0c59e8
xuchao0903 <xuchao0903@aeec31db-784d-0410-b989-a3fc7d57269e>
2008-05-21 01:35:33 +0000

    recreate this project.

---
+++ Chewthebook/app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '58d3e072df6768b35e4f00a7dca26e16'
+end
