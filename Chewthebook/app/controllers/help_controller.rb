f4e9b84acd1f89e37561d6cfd51b0b6c2ad4e6a3
xuchao0903 <xuchao0903@aeec31db-784d-0410-b989-a3fc7d57269e>
2008-05-25 14:05:40 +0000

    Added acts_as_authenticated.

--- Chewthebook/app/controllers/help_controller.rb
+++ Chewthebook/app/controllers/help_controller.rb
@@ -1,5 +1,9 @@
 class HelpController < ApplicationController
-
+  include AuthenticatedSystem
+  # If you want "remember me" functionality, add this before_filter to Application Controller
+  #before_filter :login_from_cookie
+  before_filter :login_required
+  
   def index
   end
 
