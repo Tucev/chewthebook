c7150d0a4e4160c6149c7fab555a7df8fcafc7c5
xuchao0903 <xuchao0903@aeec31db-784d-0410-b989-a3fc7d57269e>
2008-05-29 10:12:51 +0000

    Add register link.

--- trunk/Chewthebook/app/controllers/help_controller.rb
+++ trunk/Chewthebook/app/controllers/help_controller.rb
@@ -1,8 +1,8 @@
 class HelpController < ApplicationController
-  include AuthenticatedSystem
+  # include AuthenticatedSystem
   # If you want "remember me" functionality, add this before_filter to Application Controller
   #before_filter :login_from_cookie
-  before_filter :login_required
+  # before_filter :login_required
   
   def index
   end
